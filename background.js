4ebef4d1d9fc10a7b102a497be364b967037ed65
Devin Rooney <devroo2000@gmail.com>
2021-11-11 21:17:31 -0500

    Add basic whitelist auto-activate

--- background.js
+++ background.js
@@ -1,9 +1,31 @@
-// function overall deactivate
+chrome.tabs.onUpdated.addListener(function (tabId , info, tab) {
+    if (info.status === 'complete') {
+        chrome.storage.sync.get(['copyable_data'], function (result) {
+            var data = JSON.parse(result.copyable_data);
+            if(data.list.includes(tab.url)){
+                user_select_auto();
+                pointer_events_auto();
+            }
+        });
+    }
+});
 
-// function deactivate blocker 1
+function user_select_auto(){
+    var CSSInjection = "*{ user-select: auto !important; }"
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.scripting.insertCSS({
+            target: { tabId: tabs[0].id },
+            css: CSSInjection
+        });
+    });
+}
 
-// function deactivate blocker 2
-
-// function deactivate blocker 3
-
-// function save_to_list()
+function pointer_events_auto(){
+    var CSSInjection = "*{ pointer-events: auto !important; }"
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.scripting.insertCSS({
+            target: { tabId: tabs[0].id },
+            css: CSSInjection
+        });
+    });
+}
